06299d504b99f93f685a4e71e582765dccd03641
Valcinei <valcinei@live.com>
2019-07-04 18:30:53 -0400

    fix error and update version

--- src/commands/consumer.ts
+++ src/commands/consumer.ts
@@ -1,56 +1,69 @@
-
 import {
-    KafkaClient,
-    Consumer,
-    KafkaClientOptions,
-    OffsetFetchRequest
-
-} from 'kafka-node';
-import { GluegunToolbox } from 'gluegun';
+  KafkaClient,
+  Consumer,
+  KafkaClientOptions,
+  OffsetFetchRequest
+} from 'kafka-node'
+import { GluegunToolbox } from 'gluegun'
 
 module.exports = {
-    name: 'consumer',
-    alias: ['c'],
-    run: async (toolbox: GluegunToolbox) => {
-
-        const aksHost = { type: 'input', name: 'host', message: 'Whats host you broker?(default:localhost:9092)' }
-        const aksTopic = { type: 'input', name: 'topics', message: 'Whats topic you want consume?(separated by comma)' }
-        const aksPartition = { type: 'input', name: 'partition', message: 'How many partitions?(default: 0)' }
-
-        const { host, topics, partitions } = await toolbox.prompt.ask([aksHost, aksTopic, aksPartition])
-
-        let fetchRequestsList: Array<OffsetFetchRequest> = []
-        const topicList = topics.split(',')
-
-        if (topicList && topicList.length > 0) {
-            topicList.forEach((topic: any) => {
-                let newTopic = {
-                    topic: topic,
-                    partition: partitions ? Number(partitions) : 0
-                }
-                fetchRequestsList.push(newTopic)
-
-            })
-        } else {
-            console.log('Please insert your topics');
-            return
-        }
-
-        if (host) {
-          await  startConsumer(fetchRequestsList, { kafkaHost: host })
-        } else {
-          await  startConsumer(fetchRequestsList)
-        }
+  name: 'consumer',
+  alias: ['c'],
+  run: async (toolbox: GluegunToolbox) => {
+    const aksHost = {
+      type: 'input',
+      name: 'host',
+      message: 'Whats host you broker?(default:localhost:9092)'
+    }
+    const aksTopic = {
+      type: 'input',
+      name: 'topics',
+      message: 'Whats topic you want consume?(separated by comma)'
+    }
+    const aksPartition = {
+      type: 'input',
+      name: 'partition',
+      message: 'How many partitions?(default: 0)'
     }
 
+    const { host, topics, partitions } = await toolbox.prompt.ask([
+      aksHost,
+      aksTopic,
+      aksPartition
+    ])
+
+    let fetchRequestsList: Array<OffsetFetchRequest> = []
+    const topicList = topics.split(',')
+
+    if (topicList && topicList.length > 0) {
+      topicList.forEach((topic: any) => {
+        let newTopic = {
+          topic: topic,
+          partition: partitions ? Number(partitions) : 0
+        }
+        fetchRequestsList.push(newTopic)
+      })
+    } else {
+      console.log('Please insert your topics')
+      return
+    }
+
+    if (host) {
+      await startConsumer(fetchRequestsList, { kafkaHost: host })
+    } else {
+      await startConsumer(fetchRequestsList)
+    }
+  }
 }
 
-
-const startConsumer = async(topicList?: Array<OffsetFetchRequest>, options?: KafkaClientOptions, autoCommit = false) => {
-    const client = new KafkaClient(options);
-    const consumer = new Consumer(client, topicList, { autoCommit: autoCommit });
-    consumer.on('message', (message) => {
-        console.log(message);
-    });
-
+const startConsumer = async (
+  topicList?: Array<OffsetFetchRequest>,
+  options?: KafkaClientOptions,
+  autoCommit = false
+) => {
+  const client = new KafkaClient(options)
+  const consumer = new Consumer(client, topicList, { autoCommit: autoCommit })
+  consumer.on('message', message => {
+    console.log(message)
+  })
 }
--- src/commands/producer.ts
+++ src/commands/producer.ts
@@ -1,57 +1,74 @@
-
-import {
-    KafkaClient,
-    Producer,
-    KafkaClientOptions
-
-} from 'kafka-node';
-import { GluegunToolbox } from 'gluegun';
+import { KafkaClient, Producer, KafkaClientOptions } from 'kafka-node'
+import { GluegunToolbox } from 'gluegun'
 
 module.exports = {
-    name: 'producer',
-    alias: ['p'],
-    run: async (toolbox: GluegunToolbox) => {
-
-        const aksHost = { type: 'input', name: 'host', message: 'Whats host you broker?(default:localhost:9092)' }
-        const aksTopic = { type: 'input', name: 'topics', message: 'Whats topic you want produce?(separated by comma)' }
-        const aksPartition = { type: 'input', name: 'partition', message: 'How many partitions?(default: 0)' }
-        const { host, topics, partitions } = await toolbox.prompt.ask([aksHost, aksTopic, aksPartition])
-        const topicList = topics.split(',')
-
-        if (host) {
-            startProduce({ kafkaHost: host })
-        } else {
-            startProduce()
-        }
-        while (true) {
-            const Aksmsg = { type: 'input', name: 'data', message: 'Insert your message:' }
-            const msg = await toolbox.prompt.ask(Aksmsg)
-            sendMsg(topicList, msg.data, partitions)
-            msg.data = ''
-        }
+  name: 'producer',
+  alias: ['p'],
+  run: async (toolbox: GluegunToolbox) => {
+    const aksHost = {
+      type: 'input',
+      name: 'host',
+      message: 'Whats host you broker?(default:localhost:9092)'
     }
+    const aksTopic = {
+      type: 'input',
+      name: 'topics',
+      message: 'Whats topic you want produce?(separated by comma)'
+    }
+    const aksPartition = {
+      type: 'input',
+      name: 'partition',
+      message: 'How many partitions?(default: 0)'
+    }
+    const { host, topics, partitions } = await toolbox.prompt.ask([
+      aksHost,
+      aksTopic,
+      aksPartition
+    ])
+    const topicList = topics.split(',')
 
+    if (host) {
+      startProduce({ kafkaHost: host })
+    } else {
+      startProduce()
+    }
+    while (true) {
+      const Aksmsg = {
+        type: 'input',
+        name: 'data',
+        message: 'Insert your message:'
+      }
+      const msg = await toolbox.prompt.ask(Aksmsg)
+      sendMsg(topicList, msg.data, partitions)
+      msg.data = ''
+    }
+  }
 }
 
 // tslint:disable-next-line: one-variable-per-declaration
 let client = new KafkaClient(),
-    producer = new Producer(client)
+  producer = new Producer(client)
 
 const sendMsg = (topicList, dataMsg, partitions) => {
-    console.log(partitions)
-    let payloads = [];
-    topicList.forEach(topic => {
-        payloads.push({ topic: topic, messages: dataMsg, partition: partitions ? partitions : 0 })
-    });
-    producer.send(payloads, (err, dataMsg) => {
-        console.log(dataMsg);
-        console.log('Sended')
-    });
+  console.log(partitions)
+  let payloads = []
+  topicList.forEach(topic => {
+    payloads.push({
+      topic: topic,
+      messages: dataMsg,
+      partition: partitions ? partitions : 0
+    })
+  })
+  producer.send(payloads, (err, dataMsg) => {
+    if (err) {
+      console.log('Error to send', err)
+    }
+    console.log(dataMsg)
+    console.log('Sended')
+  })
 }
 
 const startProduce = (options?: KafkaClientOptions) => {
-    client = new KafkaClient(options);
-    producer = new Producer(client);
-
-
+  client = new KafkaClient(options)
+  producer = new Producer(client)
 }
